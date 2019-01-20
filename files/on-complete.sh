449feb99d70a3729db45d82827fe5e7fa642aacb
oldiy <oldiy@163.com>
2019-01-21 00:24:39 +0800

    增加

---
+++ files/on-complete.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+if [ $2 -eq 1 ]; then
+	mv "$3" /data
+fi
+echo [$(date)] $2, $3, $1 "<br>" >> /data/_log.html
---
+++ files/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ ! -f /conf/aria2.conf ]; then
+	cp /conf-copy/aria2.conf /conf/aria2.conf
+	if [ $SECRET ]; then
+		echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
+	fi
+fi
+if [ ! -f /conf/on-complete.sh ]; then
+	cp /conf-copy/on-complete.sh /conf/on-complete.sh
+fi
+
+chmod +x /conf/on-complete.sh
+touch /conf/aria2.session
+
+darkhttpd /aria2-webui/docs --port 81 &
+darkhttpd /data --port 8080 &
+darkhttpd /aria2ng --port 80 &
+aria2c --conf-path=/conf/aria2.conf
